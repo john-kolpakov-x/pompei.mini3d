75ae04c126344bff9a049446e742fd364b133a7c
pompei <john.kolpakov.x@gmail.com>
2015-02-01 13:50:28 +0600

    Подготовил текстуру для оттображения с текстурой

---
+++ pompei.mini3d/src/test/java/pompei/mini3d/probe/ProbeDrawWithTexture.java
@@ -0,0 +1,11 @@
+package pompei.mini3d.probe;
+
+public class ProbeDrawWithTexture {
+  public static void main(String[] args) throws Exception {
+    new ProbeDrawWithTexture().run();
+  }
+  
+  private void run() throws Exception {
+    
+  }
+}
